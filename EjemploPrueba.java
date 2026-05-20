5f4aec87758b280b547698cc2380b6943b5866ce
GarciaYrupailla <garciayrupailla@gmail.com>
2026-05-20 10:23:46 -0500

    feat: implementar factorial funcional con LongStream

--- EjemploPrueba.java
+++ EjemploPrueba.java
@@ -1,8 +1,8 @@
 public class EjemploPrueba {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         int numero = 5;
-        System.out.println("Prueba V2 Recursiva");
+        System.out.println("Prueba V3 Funcional");
         System.out.println("Factorial de " + numero + " = " + Factorial.calcular(numero));
     }
-
 }
